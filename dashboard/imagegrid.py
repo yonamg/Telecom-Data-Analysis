fce91c365d3c70382b07f1c46d522e65e99d0933
yonamg <softeng2006@gmail.com>
2022-08-27 19:56:56 +0300

    image saved

--- dashboard/imagegrid.py
+++ dashboard/imagegrid.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import os
+from PIL import Image
+import sys
 
 
-def imagegrid():
+def image_grid():
     st.title('Multiple Telecom-Data-Analysis Images')
-    PROJECT_PATH = os.path.abspath(os.path.dirname('../grid_image/'))
-    print(PROJECT_PATH)
-    image_dir = os.path.join(PROJECT_PATH,'images')
+    image_dir = sys.path.append(os.path.abspath(os.path.join('../grid_image/')))
+    #os.path.abspath(os.path.dirname('../grid_image/'))
+ 
     print(image_dir)
 
     fName_list = os.listdir(image_dir)
@@ -17,17 +19,17 @@ def imagegrid():
     for _ in range(len(fName_list)-1):
         cols = st.columns(4)
         if idx < len(fName_list):
-            cols[0].image(f'./images/{fName_list[idx]}',width=150, caption=fName_list[idx])
+            cols[0].image(f'./grid_image/{fName_list[idx]}',width=150, caption=fName_list[idx])
             print(os.path.join(image_dir, fName_list[idx]))
             idx += 1
         if idx < len(fName_list):
-            cols[1].image(f'./images/{fName_list[idx]}',width=150, caption=fName_list[idx])
+            cols[1].image(f'./grid_image/{fName_list[idx]}',width=150, caption=fName_list[idx])
             idx += 1
         if idx < len(fName_list):
-            cols[2].image(f'./images/{fName_list[idx]}',width=150, caption=fName_list[idx])
+            cols[2].image(f'./grid_image/{fName_list[idx]}',width=150, caption=fName_list[idx])
             idx += 1
         if idx < len(fName_list):
-            cols[3].image(f'./images/{fName_list[idx]}',width=150, caption=fName_list[idx])
+            cols[3].image(f'./grid_image/{fName_list[idx]}',width=150, caption=fName_list[idx])
             idx += 1
         else:
             break
