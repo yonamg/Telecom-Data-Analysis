ab457f55b73cd8820f5f328f0ac0a170cbbed3c6
yonamg <softeng2006@gmail.com>
2022-08-27 12:09:55 +0300

    trying deployment

--- dashboard/modelpredict.py
+++ dashboard/modelpredict.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 
 def load_model():
-    pickle_in = open('./models/satisfaction_model.pkl', 'rb')
+    pickle_in = open('../models/satisfaction_model.pkl', 'rb')
     satisfaction_model = pickle.load(pickle_in)
     return satisfaction_model
 
@@ -14,13 +14,13 @@ def load_model():
 def prdict_app():
     st.title("Prdict customer satisfaction")
 
-    eng_score = st.slider("Engagement score", min_value=0.0,
+    engage_score = st.slider("Engagement score", min_value=0.0,
                           max_value=2.0, step=0.1)
-    exp_score = st.slider("Experience score", min_value=0.0,
+    exper_score = st.slider("Experience score", min_value=0.0,
                           max_value=2.0, step=0.1)
 
     if st.button("Predict"):
         model = load_model()
-        result = model.predict([[eng_score, exp_score]])
+        result = model.predict([[engage_score, exper_score]])
 
         st.success(f"The customer satisfaction is {result[0][0]}")
--- dashboard/user_overview_page.py
+++ dashboard/user_overview_page.py
@@ -17,42 +17,42 @@ def overview_app():
     number = st.number_input("Enter the number of rows and press enter: ", min_value=None, max_value=None, value=0,
                              step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
-    df = pd.read_csv('./data/cleaned_Telecom_data.csv', nrows=number)
+    df = pd.read_csv('../data/cleaned_Telecom_data.csv', nrows=number)
     st.write(df)
 
     st.header("Top 10 handsets used by customers")
-    top_df = pd.read_csv('./data/top10handset.csv')
+    top_df = pd.read_csv('../data/top10handset.csv')
 
     fig = px.bar(top_df, x='handset_type', y='count', height=500)
     st.plotly_chart(fig)
 
     st.header("Top 3 handsets Manufacturers")
-    top_3_df = pd.read_csv('./data/top3manufacture.csv')
+    top_3_df = pd.read_csv('../data/top3manufacture.csv')
     fig = px.bar(top_3_df, x='handset_manufacturer', y='count', height=500)
     st.plotly_chart(fig)
 
     st.header("Top 5 handsets type manufactured by Huawei")
-    top_5_app = pd.read_csv('./data/top5huawei.csv')
+    top_5_app = pd.read_csv('../data/top5huawei.csv')
     fig = px.bar(top_5_app, x='Handset', y='count', height=500)
     st.plotly_chart(fig)
 
    
 
     st.header("Top 3 App Used")
-    image = Image.open('./images/top3appused.png')
+    image = Image.open('../images/top3appused.png')
     st.image(image, caption="Duration Distribution", use_column_width=True)
 
     st.header("Top 10 handset")
-    image = Image.open('./images/top10.png')
+    image = Image.open('../images/top10.png')
     st.image(image, caption="Applications Data usage", use_column_width=True)
 
     st.header("Application Duration distribution using deciles")
-    image = Image.open('./images/decile.png')
+    image = Image.open('../images/decile.png')
     st.image(image, caption="Applications Duration Distribution",
              use_column_width=True)
 
     st.header("Top 10 Satidfied customer")
-    image = Image.open('./images/top10satisfied.png')
+    image = Image.open('../images/top10satisfied.png')
     st.image(image, caption="top ten satisfied customer based on usage",
              use_column_width=True)
 
