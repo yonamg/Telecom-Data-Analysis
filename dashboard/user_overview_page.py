85f6bb0db5646285dde0345bbbf4af7aa6b55229
yonamg <softeng2006@gmail.com>
2022-08-27 11:04:24 +0300

    user_overview analysis dashboard

--- dashboard/user_overview_page.py
+++ dashboard/user_overview_page.py
@@ -1,38 +1,59 @@
-import sys
 import os
 import sys
-import numpy as np
+
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+import streamlit.components.v1 as components
+from PIL import Image
+
+sys.path.append(os.path.abspath(os.path.join('./scripts')))
 
 
-def loadDescription():
-    df = pd.read_csv("../data/Field_Descriptions.csv")
-    return df
+def overview_app():
+    st.title("Overview")
+    st.write(
+        "Customer Data's Overview")
+    number = st.number_input("Enter the number of rows and press enter: ", min_value=None, max_value=None, value=0,
+                             step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
+    df = pd.read_csv('./data/cleaned_Telecom_data.csv', nrows=number)
+    st.write(df)
 
-def loadOriginalData():
-    df = pd.read_csv("../data/Week1_challenge_data_source.csv")
-    return df
+    st.header("Top 10 handsets used by customers")
+    top_df = pd.read_csv('./data/top10handset.csv')
 
+    fig = px.bar(top_df, x='handset_type', y='count', height=500)
+    st.plotly_chart(fig)
 
-def loadPreprocessedData():
-    df = pd.read_csv("../data/cleaned_Telecom_data.csv")
-    return df
+    st.header("Top 3 handsets Manufacturers")
+    top_3_df = pd.read_csv('./data/top3manufacture.csv')
+    fig = px.bar(top_3_df, x='handset_manufacturer', y='count', height=500)
+    st.plotly_chart(fig)
 
+    st.header("Top 5 handsets type manufactured by Huawei")
+    top_5_app = pd.read_csv('./data/top5huawei.csv')
+    fig = px.bar(top_5_app, x='Handset', y='count', height=500)
+    st.plotly_chart(fig)
 
-def app():
-    st.title('TellCo Telecom Analytics Overview')
+   
 
-    st.header('Table Description')
-    st.markdown(
-    '''
-        The Telecom Dataset has 150001 observations with 55 columns. 
-        Here is the description of each columns.
-    ''')
-    df = loadDescription()
-    st.write(df, width=1200)
+    st.header("Top 3 App Used")
+    image = Image.open('./images/top3appused.png')
+    st.image(image, caption="Duration Distribution", use_column_width=True)
 
-    st.header('Here is a sample Data from the Table')
-    df = loadOriginalData()
-    st.write(df.head(10))
+    st.header("Top 10 handset")
+    image = Image.open('./images/top10.png')
+    st.image(image, caption="Applications Data usage", use_column_width=True)
+
+    st.header("Application Duration distribution using deciles")
+    image = Image.open('./images/decile.png')
+    st.image(image, caption="Applications Duration Distribution",
+             use_column_width=True)
+
+    st.header("Top 10 Satidfied customer")
+    image = Image.open('./images/top10satisfied.png')
+    st.image(image, caption="top ten satisfied customer based on usage",
+             use_column_width=True)
+
+   
