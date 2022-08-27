b6a5557d9f756f189be019bfc48879ad16759d12
yonamg <softeng2006@gmail.com>
2022-08-27 11:51:13 +0300

    changing home page style

--- dashboard/homepage.py
+++ dashboard/homepage.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as comp
 from PIL import Image
-@st.cache
-def homepage_app():
-    st.title("Home Page")
-    image = Image.open('../images/telecom.jpg')
-    st.image(image, caption="Telecom Data Analysis", use_column_width=True)
-    app_link = st.sidebar.selectbox('Select Page', ['User Overview Analysis', 'Model Prediction'])
+import user_overview_page
+import modelpredict
 
-homepage_app()
+st.title("Telecommunication Data Analysis")
+pages = {
+    "User Overview Analysis": user_overview_page,
+    "Model Prediction" : modelpredict
+}
+
+image = Image.open('../images/telecom.jpg')
+st.image(image, caption="Telecom Data Analysis", use_column_width=True)
+selection = st.sidebar.radio("Go to page ", list(pages.keys()))
+page = pages[selection]
+page.app()
