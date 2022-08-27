2c72a24633fddbaed1219da97270e4aa7cd35eda
yonamg <softeng2006@gmail.com>
2022-08-27 10:27:17 +0300

    home page app change

--- dashboard/homepage.py
+++ dashboard/homepage.py
@@ -1,19 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as comp
 from PIL import Image
-import user_overview_page
-import user_engagement_page
-import user_experience_page
-import user_satisifaction_page 
-import streamlit as st
 
-PAGES = {
-    "Data Overview": user_overview_page,
-    "User Engagement Analysis":  user_engagement_page,
-    "User Experience Analytics": user_experience_page,
-    "User Satisfaction Analysis": user_satisifaction_page
-}
-
-selection = st.sidebar.radio("Go to page", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+def homepage_app():
+    st.title("Home Page")
+    image = Image.open('./images/telecom.jpg')
+    st.image(image, caption="Telecom Data Analysis", use_column_width=True)
+    st.write("A brief analysis of Telecom data")
