2df540fa18512469b04a429c4043c9fe74bd8efa
yonamg <softeng2006@gmail.com>
2022-08-27 09:35:54 +0300

    home page of dashboard

--- dashboard/homepage.py
+++ dashboard/homepage.py
@@ -2,3 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as comp
 from PIL import Image
 
+def homepage_app():
+    st.title("Home Page")
+    image = Image.open('./images/telecom.jpg')
+    st.image(image, caption="Telecom Data Analysis", use_column_width=True )
+    st.write("detail analysis of user overview analysis, user engagement, experience and satisfaction of Telecom data")
